7b55255c003a60745f59183eddc9ec3b86df3945
Mr. Ayoung <saing.sab@gmail.com>
2018-08-23 16:59:43 +0700

    Update and rename kernel.sh to upgrade-ubuntu-kernel.sh

--- upgrade-ubuntu-kernel.sh
+++ upgrade-ubuntu-kernel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear
+mkdir kernel
+cd kernel
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-headers-4.18.3-041803_4.18.3-041803.201808180530_all.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-headers-4.18.3-041803-generic_4.18.3-041803.201808180530_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-headers-4.18.3-041803-lowlatency_4.18.3-041803.201808180530_amd64.deb
@@ -14,7 +16,4 @@ sudo dpkg -i *.deb
 echo "Successfully Install New Kernel v4.18"
 uname -sr
 
-
-
-
-
+echo "If You See Your Kernel Version Still The Same Please Reboot Your System"
