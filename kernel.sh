37724083331e10bb9e0d730a20c66481ce5f88c1
Mr. Ayoung <saing.sab@gmail.com>
2018-08-23 16:57:11 +0700

    Add files via upload

---
+++ kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-headers-4.18.3-041803_4.18.3-041803.201808180530_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-headers-4.18.3-041803-generic_4.18.3-041803.201808180530_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-headers-4.18.3-041803-lowlatency_4.18.3-041803.201808180530_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-image-unsigned-4.18.3-041803-generic_4.18.3-041803.201808180530_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-image-unsigned-4.18.3-041803-lowlatency_4.18.3-041803.201808180530_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-modules-4.18.3-041803-generic_4.18.3-041803.201808180530_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-modules-4.18.3-041803-lowlatency_4.18.3-041803.201808180530_amd64.deb
+
+chmode *.deb
+sudo dpkg -i *.deb
+
+echo "Successfully Install New Kernel v4.18"
+uname -sr
+
+
+
+
+
