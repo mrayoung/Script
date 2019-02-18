e0cbfa8dae475e71bd68a564cbf37ccc69629d28
Mr. Ayoung <saing.sab@gmail.com>
2019-02-18 09:14:58 +0700

    Update upgrade-ubuntu-kernel.sh

--- upgrade-ubuntu-kernel.sh
+++ upgrade-ubuntu-kernel.sh
@@ -10,7 +10,7 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-image-unsigned-
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-modules-4.18.3-041803-generic_4.18.3-041803.201808180530_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18.3/linux-modules-4.18.3-041803-lowlatency_4.18.3-041803.201808180530_amd64.deb
 
-chmode *.deb
+chmode +x *.deb
 sudo dpkg -i *.deb
 
 echo "Successfully Install New Kernel v4.18"
